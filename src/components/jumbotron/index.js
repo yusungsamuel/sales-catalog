428591927c45509a451c4a640cd33ed5b731a973
yusungsamuel <ssyyu@ucdavis.edu>
2019-11-06 21:59:04 -0800

    attached bootstrap cdn and made rough draft for jumbo

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Jumbotron() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Sales Catalog</h1>
+                <p class="lead">An one-stop-shop for shopping sales items from your favorite brand</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
