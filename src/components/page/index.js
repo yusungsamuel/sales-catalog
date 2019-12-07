a1fdc1fb40676266d4209056282a9efbee640a89
yusungsamuel <ssyyu@ucdavis.edu>
2019-12-06 22:49:33 -0800

    require axios. add get request to page

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,9 +1,27 @@
 import React, {Component} from "react"
+import axios from "axios"
 
 class Page extends Component {
     state = {
-
+        items: [] 
     }
+    componentDidMount(){
+        axios.get("http://localhost:3000/uniqlo")
+        .then( res => {
+            this.setState({items:res})
+        })
+    }
+    render(){
+        return (
+            this.state.items.map((item, i)=> {
+                return (
+                <h5>{item.name}</h5>
+                )
+            }
+            )
+        )
+    }
+
 }
 
 export default Page;
