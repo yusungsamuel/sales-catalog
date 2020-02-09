23ca973119b0ab3d31a877cb65d560ebc7bf885b
Your Name <you@example.com>
2020-02-09 12:27:06 -0800

    make product cards component

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,31 +1,39 @@
 import React, {Component} from "react"
 import axios from "axios"
+import ProductCard from "../productCard"
+import "./style.css"
 
 class Page extends Component {
     state = {
         items: [] 
     }
     componentDidMount(){
-        axios.get("http://localhost:8080/uniqlo/")
+        axios.get("https://enigmatic-tundra-66827.herokuapp.com/api/uniqlo")
         .then( res => {
             this.setState({items:res.data})
         })
         .catch(err => console.log(err));
     }
+
     render(){
         console.log(this.state.items)
         return (
-            this.state.items.length ? 
-            <div>
+             
+            <div className="product-display">
             {this.state.items.map((item, i)=> {
                 return (
-                <h5>Hi{item.name}</h5>
+                <ProductCard
+                    name = {item.name}
+                    link = {item.link}
+                    image = {item.image}
+                    regprice = {item.regprice}
+                    sale = {item.salesprice}
+                />
                 )
             }
             )}
             </div>
-            :
-            <div></div>
+            
         )
     }
 
---
+++ src/components/productCard/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react"
+import "./style.css"
+
+function ProductCard(props) {
+    return (
+        <div className="page-wrapper">
+            <div className="page-inner">
+                <div className="row">
+                    <div className="el-wrapper">
+                        <div className="box-up">
+                            <img className="img" src={props.image} alt="" />
+                            <div className="img-info">
+                                <div className="info-inner">
+                                    <span className="p-name">uniqlo</span>
+                                    <span className="p-company"></span>
+                                </div>
+                                <div className="a-size"><span className="size">{props.name}</span></div>
+                            </div>
+                        </div>
+
+                        <div className="box-down">
+                            <div className="h-bg">
+                                <div className="h-bg-inner"></div>
+                            </div>
+
+                            <a className="cart" href={props.link}>
+                                <span className="regprice">{props.regprice}</span>
+                                <span className="saleprice">{props.sale}</span>
+                                <span className="add-to-cart">
+                                    <span className="txt">Sizes and Colors</span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
