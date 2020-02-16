e2eb6ddef3073b9ddd30e505e94d6fcc5dac2df1
yusungsamuel <ssyyu@ucdavis.edu>
2020-02-16 15:25:55 -0800

    reorganize structure of sideForm and page. Convert input component to stateful component

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,40 +1,70 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import axios from "axios"
 import ProductCard from "../productCard"
 import "./style.css"
+import brands from "../../brands.json"
+import {SideForm, Input} from "../sideForm"
 
 class Page extends Component {
     state = {
-        items: [] 
+        brands: [],
+        items: []
     }
-    componentDidMount(){
+    componentDidMount() {
         axios.get("https://enigmatic-tundra-66827.herokuapp.com/api/uniqlo")
-        .then( res => {
-            this.setState({items:res.data})
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                this.setState({ items: res.data })
+            })
+            .catch(err => console.log(err));
     }
 
-    render(){
+    handleBrandOption = (event) => {
+        event.preventDefault();
+        event.target.checked = !event.target.checked
+    }
+
+    render() {
         console.log(this.state.items)
         return (
-             
-            <div className="product-display">
-            {this.state.items.map((item, i)=> {
-                return (
-                <ProductCard
-                    name = {item.name}
-                    link = {item.link}
-                    image = {item.image}
-                    regprice = {item.regprice}
-                    sale = {item.salesprice}
-                    brand = {item.brand}
-                />
-                )
-            }
-            )}
+
+            <div className="row">
+                <div className="col-3">
+                    <SideForm>
+                        {
+                            brands.map((brand, i) => {
+                                return (
+                                    <Input
+                                        name={brand.name}
+                                        key={i}
+                                        num={i}
+                                        handleBrandClick = {this.handleBrandOption}
+                                    />
+                                )
+                            })
+                        }
+                    </SideForm>
+                </div>
+                <div className="col-9 center product-display">
+                    {this.state.items.map((item, i) => {
+                        return (
+                            <ProductCard
+                                name={item.name}
+                                link={item.link}
+                                image={item.image}
+                                regprice={item.regprice}
+                                sale={item.salesprice}
+                                brand={item.brand}
+                                key = {i}
+                            />
+                        )
+                    }
+                    )}
+                </div>
             </div>
-            
+
+
+
+
         )
     }
 
--- src/components/sideForm/index.js
+++ src/components/sideForm/index.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react"
 import "./style.scss"
+
 export function SideForm(props) {
     return (
-        <form className="form">
+        <form className="form" >
             {props.children}
         </form>
     )
-}
-
-export function Input(props) {
-    return (
-        <div className="inputGroup">
-            <input id={"option" + props.num} name={"option" + props.num} type="checkbox" />
-            <label for={"option" + props.num}>{props.name}</label>
-        </div>
-    )
 
 }
+
+
+export class Input extends Component {
+    state = {
+        checked: false
+    }
+
+    handleChange = (event) => {
+
+        this.setState({ checked: !this.state.checked })
+        console.log(event.target.checked)
+    }
+
+    render() {
+        return (
+            <div className="inputGroup">
+                <input id={"option" + this.props.num} name={"option" + this.props.num} type="checkbox" checked={this.state.checked} onChange={this.handleChange} />
+                <label for={"option" + this.props.num}>{this.props.name}</label>
+            </div>
+        )
+    }
+}
+
 
