04519d5fcfe02f8495636e2a0a261a1fe3d89aaf
Your Name <you@example.com>
2020-03-11 16:19:04 -0700

    add about page

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -5,9 +5,13 @@ function About() {
     return (
         <div>
             <div className="row about">
-                <div className="col-6 gif"></div>
-                <div className="col-6">
+                <div className="col-6 about-left"></div>
+                <div className="about-right col-6">
+                    <div className="description mx-5">
+                    <header>Sales Calatalog</header>
                     <p>Save Time! Save Money! Sales catalog is an one-stop "shop" for your to browse on sale items from your favorite brands. We collect re-time on sale item information from these fashion brands and display it all on one page.</p>
+                    <p>Can't find your favorite brand? Stay tuned! They will be added soon.</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 function Nav() {
     return (
-        <ul className="nav nav-bar mb-5">
+        <ul className="nav nav-bar mb-5 sticky-top">
             <li className="nav-item">
                 <a className="nav-link name" href="/">SALES CATALOG</a>
             </li>
